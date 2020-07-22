105a2a6090538819acce976114159cf125d22b39
qasimqlf <qlfqasim@gmail.com>
2020-07-22 17:51:55 +0500

    Add existing file

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Pages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookfromDb = await _db.Book.FindAsync(Book.Id);
+                BookfromDb.Name = Book.Name;
+                BookfromDb.Author = Book.Author;
+                BookfromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
---
+++ BookListRazor/Pages/BookList/index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Pages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class indexModel : PageModel
+    {
+        //this way to can extract the Application db context 
+        //that is inside the dependancy inject container and injected on to this page.
+        
+        private readonly ApplicationDbContext _db;
+
+        public indexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        //if we use async and await then return type would be 'async task', not void
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+
+        }
+
+    }
+}
